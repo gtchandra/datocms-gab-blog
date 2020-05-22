bcdd89ed05d2797bbe3892d603c960a8c03b0854
gab <>
2020-05-22 11:05:06 +0200

    restored again

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -9,6 +9,7 @@ export default async (req, res) => {
   ) {
     return res.status(401).json({ message: 'Invalid token' })
   }
+  
 
   // Fetch the headless CMS to check if the provided `slug` exists
   const post = await getPreviewPostBySlug(req.query.slug)
